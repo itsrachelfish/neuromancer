8ff490196d5bb941141e1f21651764692fc9a8e9
Edwin Pers <pers.edwin@gmail.com>
2015-02-22 15:19:08 -0500

    remind works properly now yay

--- modules/default/remind.js
+++ modules/default/remind.js
@@ -35,60 +35,66 @@ var remind = {
 
     if (args[0].toLowerCase() == "-f") {
       setTimeout(function() {
-        var to_say = from + ": " + color.blue(args.slice(2).join(' '));
-        remind.core.send("say", from, to, to_say);
+        var to_say = from.toLowerCase() + ": " + color.blue(args.slice(2).join(' '));
+        remind.core.send("say", from.toLowerCase(), to, to_say);
       }, ptime);
     } else {
-      if (!remind.core.databases.remind[from]) {
-        remind.core.databases.remind[from] = [];
+      if (!remind.core.databases.remind[from.toLowerCase()]) {
+        remind.core.databases.remind[from.toLowerCase()] = [];
       }
-      
-      remind.core.databases.remind[from].push({
+
+      remind.core.databases.remind[from.toLowerCase()].push({
         t: time,
         m: args.slice(1).join(' '),
       });
     }
-    
+
     // as with tell, enable this if your bot is flakey
     remind.core.write_db("remind");
   },
-  
+
   listener: function(from, to, message) {
-    if (remind.core.databases.remind[from]) {
-      remind.core.databases.remind[from].forEach(function(entry, index, object) {
+    if (remind.core.databases.remind[from.toLowerCase()]) {
+      var save = false;
+      remind.core.databases.remind[from.toLowerCase()].forEach(function(entry, index, object) {
         if (entry.t <= Date.now()) {
-          remind.client.send("say", from, to, from + ": " + color.blue(entry.m));
+          remind.core.send("say", from, to, from + ": " + color.blue(entry.m));
           object.splice(index, 1);
+          save = true;
         }
       });
-     remind.core.write_db("remind"); 
+      if (save) {
+        remind.core.write_db("remind");
+      }
     }
-  }, 
-  
+  },
+
   bind: function() {
     remind.client.addListener("message", remind.message);
+    remind.client.addListener("message", remind.listener);
   },
-  
+
   unbind: function() {
     remind.client.removeListener("message", remind.message);
+    remind.client.addListener("message", remind.listener);
   }
-  
+
 };
 
 module.exports = {
   load: function(core) {
     remind.core = core;
     remind.client = remind.core.client;
-    
+
     remind.core.read_db("remind");
     remind.bind();
   },
-  
+
   unload: function() {
     remind.unbind();
     remind.core.write_db("remind");
     delete remind;
   },
-  
+
   commands: remind.commands
 };
