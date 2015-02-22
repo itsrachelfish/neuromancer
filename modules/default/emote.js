ebd0dabbfa3d4ebe3f8d6a14fe99ed0719063986
Edwin Pers <pers.edwin@gmail.com>
2015-02-22 16:21:51 -0500

    made emote a bit easier to read

--- modules/default/emote.js
+++ modules/default/emote.js
@@ -62,15 +62,15 @@ var emote = {
 
     if (y >= 750) {
       var dbladez = [
-        '\x03\x02illegal dbladez\x02',
-        '\x03\x02I snuck dbladez into prison up my ass.\x02',
-        '\x03\x02I love sniffing whole lines of dbladez.\x02',
-        '\x03\x02Twenty-five years in prison was worth it for just one hit of dbladez\x02',
-        '\x03\x02Taking dbladez ruined my life.\x02'
+        'illegal dbladez',
+        'I snuck dbladez into prison up my ass.',
+        'I love sniffing whole lines of dbladez.',
+        'Twenty-five years in prison was worth it for just one hit of dbladez',
+        'Taking dbladez ruined my life.'
       ];
-      emote.core.send("say", from, to, dbladez[x]);
+      emote.core.send("say", from, to, color.bold(dbladez[x]));
     } else {
-      emote.core.send("say", from, to, "\x03\x02illegal drugs\x02");
+      emote.core.send("say", from, to, color.bold("illegal drugs"));
     }
   },
 
@@ -78,18 +78,18 @@ var emote = {
     var x = ~~ (Math.random() * 29) + 0;
 
     if (x == 9) {
-      emote.core.send("say", from, to, "\x03\x02There are no legal drugs.\x02");
+      emote.core.send("say", from, to, color.bold("There are no legal drugs."));
     } else if (x == 19) {
-      emote.core.send("say", from, to, "\x03\x02All drugs are illegal.\x02");
+      emote.core.send("say", from, to, color.bold("All drugs are illegal."));
     } else if (x == 29) {
-      emote.core.send("say", from, to, "\x03\x02Your drug use has been logged and reported.\x02");
+      emote.core.send("say", from, to, color.bold("Your drug use has been logged and reported."));
     } else {
-      emote.core.send("say", from, to, "\x03\x02legal drugs\x02");
+      emote.core.send("say", from, to, color.bold("legal drugs\x02"));
     }
   },
 
   intense: function(from, to, message) {
-    emote.core.send("say", from, to, "\x03\x02[" + message + " intensifies]\x02");
+    emote.core.send("say", from, to, color.bold("[" + message + " intensifies]"));
   },
 
   listener: function(from, to, message) {
@@ -97,12 +97,12 @@ var emote = {
       var x = ~~ (Math.random() * 4) + 0;
       var y = ~~ (Math.random() * 99) + 0;
 
-      if (y == 99) {
+      if (y > 88) {
         emote.core.send("say", from, to, "hi every1 im new!!!!!!! holds up spork my name is katy but u can call me t3h PeNgU1N oF d00m!!!!!!!! lol…as u can see im very random!!!! thats why i came here, 2 meet random ppl like me _… im 13 years old (im mature 4 my age tho!!) i like 2 watch invader zim w/ my girlfreind (im bi if u dont like it deal w/it) its our favorite tv show!!! bcuz its SOOOO random!!!! shes random 2 of course but i want 2 meet more random ppl =) like they say the more the merrier!!!! lol…neways i hope 2 make alot of freinds here so give me lots of commentses!!!! DOOOOOMMMM!!!!!!!!!!!!!!!! <--- me bein random again _^ hehe…toodles!!!!!");
         emote.core.send("say", from, to, "loves and waffles,");
         emote.core.send("say", from, to, "t3h PeNgU1N oF d00m");
       } else {
-        if (y < 49) {
+        if (y < 55) {
           var xd = ['xd', 'xD', 'XD', 'xDD', 'XDD'];
           emote.core.send("say", from, to, xd[x]);
         }
