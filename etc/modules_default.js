46bd8c5c005be7f0fe213c4d501623baaf70172c
Edwin Pers <pers.edwin@gmail.com>
2015-02-23 00:05:50 -0500

    derp

--- etc/modules_default.js
+++ etc/modules_default.js
@@ -1,1 +1,1 @@
-module.exports = ["admin", "away", "calc", "cleverbot", "echo", "emote", "flip", "help", "lastfm", "prefix", "py", "remind", "roll", "search", "seen", "tell", "translate", "url", "wa", "weather"];
+module.exports = ["admin", "away", "calc", "cowsay", "cleverbot", "echo", "emote", "flip", "help", "lastfm", "prefix", "py", "remind", "roll", "search", "seen", "tell", "translate", "url", "wa", "weather"];
