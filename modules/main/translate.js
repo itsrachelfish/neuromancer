aaae905985e303f9a5c405b321b476cf6279bcff
Edwin Pers <pers.edwin@gmail.com>
2015-02-26 23:42:37 -0500

    reformatted translate

--- modules/main/translate.js
+++ modules/main/translate.js
@@ -7,7 +7,7 @@ var translate = {
 
   translate: function(from, to, message) {
     request('http://translate.google.com/translate_a/t?client=t&sl=auto&tl=en&q=' + message, function(e, r, body) {
-      translate.core.send("say", from, to, from + ': [\u000304Translation \u000310]' + JSON.parse(body.replace(/,,/g, ',null,').replace(/,,/g, ',').replace(/\[,/g, '[null,'))[0][0][0])
+      translate.core.send("say", from, to, from + '[' + color.green("translation") + ']' + JSON.parse(body.replace(/,,/g, ',null,').replace(/,,/g, ',').replace(/\[,/g, '[null,'))[0][0][0])
     });
   },
 
