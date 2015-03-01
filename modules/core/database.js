9f9f65f69ac046c3dc20bec1393e7896c24fe797
Edwin Pers <pers.edwin@gmail.com>
2015-03-01 00:57:00 -0500

    send/recieve and database ops are modules now

---
+++ modules/core/database.js
@@ -0,0 +1,47 @@
+var fs = require("fs");
+
+var database = {
+  core: false,
+  
+  read_db: function(db) {
+    var path = "./db/" + db + ".db";
+    fs.readFile(path, function(err, data) {
+      if (err) {
+        console.error("[ERROR][db]: ".red + db + " database could not be read.");
+        console.error(path);
+        console.error(err);
+        console.error("[ERROR][db]: ".red + "the database should be created automatically if it doesn't exist");
+        return;
+      }
+      database.core.databases[db] = JSON.parse(data, "utf8");
+      console.log("[db]: ".blue + db + " database loaded.");
+    });
+  },
+
+  write_db: function(db) {
+    var path = "./db/" + db + ".db";
+    fs.writeFile(path, JSON.stringify(database.core.databases[db]), "utf8", function(err) {
+      if (err) {
+        console.error("[ERROR][db]: ".red + db + " database could not be written.");
+        console.error(path);
+        console.error(err);
+        return;
+      }
+      console.log("[db]: ".blue + db + " database saved.");
+    });
+  }
+}
+
+module.exports = {
+  load: function(core) {
+    database.core = core;
+    database.core.read_db = database.read_db;
+    database.core.write_db = database.write_db;
+  },
+  
+  unload: function() {
+    database.core.read_db = false;
+    database.core.write_db = false;
+    delete database;
+  }
+}
---
+++ modules/core/messages.js
@@ -0,0 +1,67 @@
+var messages = {
+  core: false,
+  client: false,
+
+  send: function(type, from, to, message) {
+    //determine if it's a channel message or a privmessage
+    if (to.charAt(0) == '#') {
+      messages.core.client[type](to, message);
+    } else {
+      messages.core.client[type](from, message);
+    }
+  },
+
+  recieve: function(module_id, from, to, text, details) {
+    var userhost = details.user + '@' + details.host;
+    // if the command is prefixed with our command prefix
+    if (text.charAt(0) == messages.core.config.prefix) {
+      text = text.substr(1);
+      text = text.split(' ');
+
+      var command = text.shift();
+
+      // If the module is loaded and the command is valid
+      if (typeof messages.core.loaded[module_id] != "undefined" && messages.core.loaded[module_id].commands.indexOf(command) > -1) {
+
+        // if the ignore module is loaded
+        if (messages.core.loaded["main/ignore"]) {
+          var ignore = false;
+          if (messages.core.databases.ignore[from.toLowerCase()]) {
+            messages.core.databases.ignore[from.toLowerCase()].forEach(function(entry, index, object) {
+              if (entry == module_id.split('/')[1]) {
+                console.log("[ignore]:".yellow + " ignored command '" + command + ' ' + text.join(' ') + "' from '" + from + "'");
+                ignore = true;
+              }
+            });
+          }
+          if (ignore) {
+            return;
+          }
+        }
+
+        // if it's an admin-only module and the user is a non-admin
+        if (messages.core.loaded[module_id].admin && userhost != messages.core.config.owner) {
+          return;
+        }
+
+        // run the command
+        text = text.join(' ');
+        messages.core.loaded[module_id].run(command, from, to, text);
+      }
+    }
+  }
+}
+
+module.exports = {
+  load: function(core) {
+    messages.core = core;
+    messages.core.send = messages.send;
+    messages.core.recieve = messages.recieve;
+  },
+
+  unload: function() {
+    messages.core.send = false;
+    messages.core.recieve = false;
+    delete messages;
+  }
+}
