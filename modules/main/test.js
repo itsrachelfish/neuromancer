e1ce3554b984e6c941cfd4552a6e24f585978f27
Edwin Pers <pers.edwin@gmail.com>
2015-03-17 22:33:27 -0400

    testing sms sending

---
+++ modules/main/test.js
@@ -0,0 +1,32 @@
+var sms = require("mtextbelt");
+
+var test = {
+  commands: ["textme"],
+  core: false,
+
+  textme: function(from, to, message) {
+    sms.send(5186451982, "test niggahhh", function(err, result) {
+      if (err) {
+        test.core.send(from, to, "error");
+      } else {
+        test.core.send(from, to, "sent");
+      }
+    })
+  }
+};
+
+module.exports = {
+  load: function(core) {
+    test.core = core;
+  },
+
+  unload: function() {
+    delete test;
+  },
+  
+  commands: test.commands,
+  run: function(command, from, to, message) {
+    test[command](from, to, message);
+  },
+  admin: true,
+};
