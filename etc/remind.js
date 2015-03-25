ea77620535cf6096691d356ba88c6f9f793210da
Edwin Pers <epers@primesli.me>
2015-03-25 12:34:01 +0000

    derp

--- etc/remind.js
+++ etc/remind.js
@@ -1,3 +1,3 @@
 module.exports = {
-  localTZ = 1; // set to the local time offset where to bot is run (ex: neuromancer lives in denmark, so we set it to 1 because denmark is UTC+1)
+  localTZ: 1, // set to the local time offset where to bot is run (ex: neuromancer lives in denmark, so we set it to 1 because denmark is UTC+1)
 }
