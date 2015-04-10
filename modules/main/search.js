cb1c028ec4143628094d6d25f2b05be02ce4e184
Edwin Pers <epers@primesli.me>
2015-04-10 09:01:06 -0400

    disabled safesearch (maybe) and enabled user notification when the api call fails

--- modules/main/search.js
+++ modules/main/search.js
@@ -1,18 +1,20 @@
 var request = require("request");
-var debug = true;
+var debug = false;
 var core;
 
 var search = {
   commands: ["g", "google"],
 
   google: function(from, to, message) {
-    request("http://ajax.googleapis.com/ajax/services/search/web?v=1.0&rsz=1&safe=oss&q=" + message, function(e, r, body) {
+    request("http://ajax.googleapis.com/ajax/services/search/web?v=1.0&rsz=1&q=" + message, function(e, r, body) {
       if (body) {
         if (debug) {
           console.log(JSON.stringify(JSON.parse(body)));
           console.log(JSON.stringify(JSON.parse(body).responseData.results[0].unescapedUrl));
         }
         core.say(from, to, from + ": " + JSON.parse(body).responseData.results[0].unescapedUrl);
+      } else {
+	  core.say(from, to, from + ": The api call failed, please try again");
       }
     });
   },
