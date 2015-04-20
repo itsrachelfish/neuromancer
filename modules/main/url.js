5b5ebd5d8d5e456a9592c25e533ee90647209523
Edwin Pers <epers@primesli.me>
2015-04-20 12:41:49 -0400

    this should work...

--- modules/main/url.js
+++ modules/main/url.js
@@ -30,7 +30,7 @@ var url = {
         console.log(message);
       }
       var ignore = false
-      // if we're ignoring the person posting the link
+        // if we're ignoring the person posting the link
       if (core.databases.ignore[from.toLowerCase()]) {
         core.databases.ignore[from.toLowerCase()].forEach(function(entry, index, object) {
           if (entry == "url") {
@@ -42,7 +42,7 @@ var url = {
       if (ignore) {
         return;
       }
-      
+
       // but not youtube links because those are handled seperately in youtube module
       if (message.search(/youtu((.be)|(be.com))/) !== -1) {
         return
@@ -54,28 +54,35 @@ var url = {
       var ua = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0'
       }
-      var body = ''
       request({
         url: link,
         encoding: 'utf8',
-        headers: ua
-      }).on('data', function(chunk) {
-        body += chunk
-        if (body.length > 64000) this.abort()
-      }).on('end', function() {
-        try {
-          var title = body.match(/<title>[\s\S]*?<\/title>/)[0].slice(7, -8)
-          title = title.replace(/&#[0-9]*;/g, function(c) {
-            return String.fromCharCode(c.match(/[0-9]+/)[0])
-          })
-          title = title.replace(/&[a-z#0-9]*;/g, function(c) {
-            return HTMLchars[c] || c
-          })
-          title = title.replace(/(\r)|(\n)/g, '').replace(/\s+/g, ' ')
-          title = title.slice(0, 80).replace(/(^\s)|(\s$)/g, '')
-          url.core.send("say", from, to, '[\u000310 ' + title + ' \u000f] -\u000304 ' + urllib.parse(link).host)
-        } catch (err) {}
-      })
+        headers: ua,
+      }, function(e, r, body) {
+        if (debug) {
+          console.log("e:" + e);
+          console.log("r:" + r);
+          console.log("body:" + body);
+        }
+        if (body) {
+          try {
+            var title = body.match(/<title>[\s\S]*?<\/title>/)[0].slice(7, -8)
+            title = title.replace(/&#[0-9]*;/g, function(c) {
+              return String.fromCharCode(c.match(/[0-9]+/)[0])
+            })
+            title = title.replace(/&[a-z#0-9]*;/g, function(c) {
+              return url.HTMLchars[c] || c
+            })
+            title = title.replace(/(\r)|(\n)/g, '').replace(/\s+/g, ' ')
+            title = title.slice(0, 80).replace(/(^\s)|(\s$)/g, '')
+            url.core.send("say", from, to, '[\u000310 ' + title + ' \u000f] -\u000304 ' + urllib.parse(link).host)
+          } catch (err) {
+            if (debug) {
+              console.log(err);
+            }
+          }
+        }
+      });
     }
   }
 };
