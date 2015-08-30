64ad96e71bfb3398a564c41f44fac7189379a573
Edwin Pers <pers.edwin@gmail.com>
2015-08-29 20:49:27 -0400

    cleared old appspot api code from wa module

--- modules/main/wa.js
+++ modules/main/wa.js
@@ -10,15 +10,7 @@ var wa = {
   },
 
   wolfram: function(from, to, message) {
-    request('http://tumbolia.appspot.com/wa/' + encodeURIComponent(message.replace('+', '%2B')), function(e, r, body) {
-      var answer = body.replace(/&times;/g, ' * ').replace(/&pound;/, '£').replace(/&amp;/g, '&').replace(/&not;/g, '¬').replace(/&gt;/g, '>').replace(/&deg;/g, '°').replace(/&[a-zA-Z]+;/g, '[symbol]').replace('\n', '').replace('\\', '').split(';')
-      if (answer[0].indexOf("Couldn't grab") != -1 || answer == []) {
-        core.say(from, to, '[' + color.red("error") + "] Couldn't display answer");
-      } else {
-        var to_say = '[' + color.blue("WolframAlpha") + '] ' + color.green(answer[0]) + ' = ' + color.red(answer[1]);
-        core.say(from, to, to_say);
-      }
-    });
+    core.say (from, from, "Sorry, but the wolfram module is currently disabled as the api is uses is changed.");
   }
 };
 
