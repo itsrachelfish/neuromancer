913adf0bdabecd73694d8d5d8fc6bd0985ea68d0
Edwin Pers <pers.edwin@gmail.com>
2015-10-26 20:02:38 -0400

    added secrets module, will support storage of api keys and etc for future use

---
+++ modules/main/secrets.js
@@ -0,0 +1,46 @@
+var secrets = {
+  commands: ["secret"],
+  
+  secret: function(from, to, message) {
+    var args = parseArgs(message.split(' '), opts={
+      boolean: ['l'],
+      string: ['a'],
+    });
+    
+    if (args.l) {
+      core.databases.secrets.forEach(function(entry, index, object) {
+        core.say(from, from, "[" + index + "] " + entry.key + " = " + entry.data);
+      });
+      return;
+    } else if (args.d) {
+      delete core.databases.secrets[args.d];
+    } else if (args.a) {
+      var key = args.a.split("=")[0];
+      var data = args.a.split("=")[1];
+      if (debug) {
+        console.log(key + " = " + data);
+      }
+      core.databases.secrets[key] = {key, data}
+      core.write_db("secrets");
+    }
+  }
+};
+
+module.exports = {
+  load: function(_core) {
+    core = _core;
+    return;
+  },
+  
+  unload: function() {
+    delete secrets;
+    delete core;
+  },
+  
+  commands: secrets.commands,
+  db: true,
+  run: function(command, from, to, message) {
+    secrets[command](from, to, message);
+    return;
+  }
+};
