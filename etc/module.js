274512a2653820ce2231ba63c3aaec160aa6bbb6
Edwin Pers <pers.edwin@gmail.com>
2015-10-29 04:40:44 -0400

    Update module.js

--- etc/module.js
+++ etc/module.js
@@ -1,4 +1,4 @@
 module.exports = {
   core: ["log", "prompt", "database", "err", "messages"],
-    main: ["admin", "away", "calc", "cleverbot", "cowsay", "emote", "flip", "help", "ignore", "lastfm", "prefix", "py", "remind", "roll", "search", "seen", "tell", "translate", "topic", "wa", "weather", "youtube", "url"]
+  main: ["secrets", "admin", "away", "calc", "cleverbot", "cowsay", "emote", "flip", "help", "ignore", "lastfm", "prefix", "py", "remind", "roll", "search", "seen", "tell", "translate", "topic", "wa", "weather", "youtube", "url"]
 }
