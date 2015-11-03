5c863d77875d02be4ca7cb528a05c8649fbc866e
Edwin Pers <pers.edwin@gmail.com>
2015-11-02 22:50:25 -0500

    derp derp i totally know how ternary operators work

--- modules/main/weather.js
+++ modules/main/weather.js
@@ -35,7 +35,7 @@ var weather = {
     }
 
     // nested ternary operators fuck yeah
-    request(weather.weathAPI + (args._[0] ? (args.s ? "weather?type=like&q=" : "weather?zip=") : ("weather?id=" + core.databases.weather[from.toLowerCase()].cityID)) + "&units=" + core.databases.weather[from.toLowerCase()].locale + "&APPID=" + core.databases.secrets.OWMAPIKey, function (e, r, body) {
+    request(weather.weathAPI + (args._[0] ? (args.s ? "weather?zip=" : "weather?type=like&q=") : ("weather?id=" + core.databases.weather[from.toLowerCase()].cityID)) + "&units=" + core.databases.weather[from.toLowerCase()].locale + "&APPID=" + core.databases.secrets.OWMAPIKey, function (e, r, body) {
       if (body) {
         if (debug) {
           console.log(body);
