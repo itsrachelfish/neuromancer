e6802baf0aa0838aa848d06a3fb45425657cccac
Edwin Pers <pers.edwin@gmail.com>
2015-12-19 06:15:14 -0500

    this is why we can have nice things

--- modules/main/eve.js
+++ modules/main/eve.js
@@ -8,6 +8,8 @@ var eve = {
     request("https://api.eveonline.com/server/ServerStatus.xml.aspx/", function (e, r, b) {
       if (b.match(/True/i)) {
         core.say(from, to, "Server is up");
+      } else {
+        core.say(from, to, "Server is down");
       }
     });
   }
