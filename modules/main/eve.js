05096e9364d388c8d31ff41cd9ff45ea34c8f36f
Edwin Pers <pers.edwin@gmail.com>
2015-12-19 06:09:59 -0500

    made a thing to report eve server status

---
+++ modules/main/eve.js
@@ -0,0 +1,33 @@
+var core;
+
+var eve = {
+  commands: ["eve"],
+
+  eve: function (from, to, message) {
+    request("https://api.eveonline.com/server/ServerStatus.xml.aspx/", function (e, r, b) {
+      if (debug) {
+        console.log(b);
+      }
+
+      if (b.match(/True/i)) {
+        core.say(from, to, Server is up);
+      }
+    });
+  }
+};
+
+module.exports = {
+  load: function (_core) {
+    core = _core;
+  },
+
+  unload: function () {
+    delete eve;
+    delete core;
+  },
+
+  commands: eve.commands,
+  run: function (command, from, to, message) {
+    eve[command](from, to, message);
+  }
+};
